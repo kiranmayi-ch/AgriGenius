89cd598814eb88a09b49ebe202f65ead9e9dc0b0
kiranmayi chukkala <kiranmayichukkala@gmail.com>
2025-12-06 20:23:50 +0000

    Try fixing this error: `Console Error: A tree hydrated but some attribut

--- src/components/main-nav.tsx
+++ src/components/main-nav.tsx
@@ -48,16 +48,16 @@ export function MainNav() {
     <SidebarMenu>
       {menuItems.map((item) => (
         <SidebarMenuItem key={item.href}>
-          <Link href={item.href} passHref>
             <SidebarMenuButton
-              as="a"
+              asChild
               isActive={pathname.startsWith(item.href)}
               tooltip={item.tooltip}
             >
-              <item.icon />
-              <span>{item.label}</span>
+              <Link href={item.href}>
+                <item.icon />
+                <span>{item.label}</span>
+              </Link>
             </SidebarMenuButton>
-          </Link>
         </SidebarMenuItem>
       ))}
     </SidebarMenu>
